d3fa4c185aa249e3e8fc4d8318b52d003644da13
dawidd4 <dawid.karwat@gmail.com>
2018-12-14 11:46:59 +0100

    zmiany

---
+++ app/DAO/postDAO.js
@@ -0,0 +1,57 @@
+'use strict';
+
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+
+import mongoConverter from '../service/mongoConverter';
+import applicationException from '../service/applicationException';
+
+const postSchema = new mongoose.Schema({
+    title: {type: String},
+    url: {type: String},
+    content: {type: String},
+}, {
+    collection: 'post'
+});
+postSchema.plugin(uniqueValidator);
+
+const PostModel = mongoose.model('post', postSchema);
+
+function query() {
+    return PostModel.find({}).then(function (result) {
+        if (result) {
+            return mongoConverter(result);
+        }
+    });
+}
+
+function get(id) {
+    return PostModel.findOne({_id: id}).then(function (result) {
+        if (result) {
+            return mongoConverter(result);
+        }
+    });
+}
+
+function createNewOrUpdate(data) {
+
+        if (!data.id) {
+            return new PostModel(data).save().then(result => {
+                if (result) {
+                    return mongoConverter(result);
+                }
+            });
+        } else {
+            return PostModel.findOneAndUpdate(data.id, _.omit(data, 'id'), {new: true});
+        }
+
+}
+
+
+export default {
+    query: query,
+    get: get,
+    createNewOrUpdate: createNewOrUpdate,
+
+    model: PostModel
+};
